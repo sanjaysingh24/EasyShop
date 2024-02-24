40bfdf4003ecf6e17a5dfa8f6d2914dade1d25f8
sanju <sanjubora84@gmail.com>
2024-02-24 21:52:15 +0530

    commit

--- src/components/Main_banner/Main.jsx
+++ src/components/Main_banner/Main.jsx
@@ -5,9 +5,9 @@ const Main = () => {
     <>
     <section>
     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active" data-bs-interval="10000">
-      <img src="./img/Banner-1.webp" class="d-block w-100" alt="..."/>
+  <div className="carousel-inner">
+    <div className="carousel-item active" data-bs-interval="10000">
+      <img src="./img/Banner-1.webp" className="d-block w-100" alt="..."/>
     </div>
 
   </div>
